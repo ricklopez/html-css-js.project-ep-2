2814be71ecbc4d18dda106823ec60620165f6219
Rick Lopez <ricklopezmba@outlook.com>
2019-02-16 13:13:38 -0600

    ep.2 complete

--- js/main.js
+++ js/main.js
@@ -7,28 +7,11 @@ fetch('http://5c5f1d0b3668970014fd1c04.mockapi.io/api/video')
         return response.json();
     })
     .then(function(myJson) {
-        console.log(myJson);
         data = myJson;
-        updateUI('video-', data);
-        console.log(data.length);
+        data.targetId = "#gridMain";
+        updateUI([createVideoCard], [data]);
     });
 
-// 2. Use that data on a web page
 
-function updateUI(className, data){
-    debugger;
-    // grab all img
-    var imgs = Array.from(document.getElementsByClassName(className + 'img'));
-    // change the img
-    imgs.forEach(function(item, index){
-        item.src = data[index].avatar;
-    });
 
-    // grab all name
-    var names = Array.from(document.getElementsByClassName(className + 'name'));
-    // change the name
-    // imgs.forEach(function(item, index){
-    //     item.src = data[index].avatar;
-    // });
-}
 
